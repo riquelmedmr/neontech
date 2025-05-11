f21cf6b6682cd79d78a8c97b2c79a76ccb602c0b
riquelmedmr <73765375+riquelmedmr@users.noreply.github.com>
2025-05-11 03:32:20 -0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+export default {
+  content: ['./index.html', './src/**/*.{js,jsx}'],
+  theme: {
+    extend: {
+      colors: {
+        neon: '#00ffe0',
+        black: '#0f0f0f',
+        pink: '#ff00c8'
+      }
+    }
+  },
+  plugins: [],
+}
