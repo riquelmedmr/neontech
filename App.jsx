abb499557ec84d94a1dae7ed86754d363d2a5754
riquelmedmr <73765375+riquelmedmr@users.noreply.github.com>
2025-05-11 03:34:26 -0300

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import Navbar from './components/Navbar'
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sobre" element={<About />} />
+        <Route path="/contato" element={<Contact />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
